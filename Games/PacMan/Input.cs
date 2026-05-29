6568e22c937608d814f66a62edc72b55ca83ca62
Cybercom682 <102969748+Cybercom682@users.noreply.github.com>
2026-05-29 19:29:24 +0200

    updated stubs , add nro

--- Games/PacMan/Input.cs
+++ Games/PacMan/Input.cs
@@ -10,9 +10,43 @@ public enum NpadButton
     StickRUp, StickRDown, StickRLeft, StickRRight,
 }
 
+public struct StickPos
+{
+    public int x;
+    public int y;
+}
+
+/// Touch-Zustand: bis zu 10 simultane Berührungen, x[i]/y[i] in Pixel (0..1280 / 0..720).
+public struct TouchState
+{
+    public int count;
+    public int[] x;
+    public int[] y;
+    public uint[] id;
+
+    /// Gibt true zurück wenn Finger idx innerhalb von (rx, ry, rw, rh) liegt.
+    public bool HitRect(int idx, int rx, int ry, int rw, int rh) => false;
+
+    /// Erster Finger getippt?
+    public bool IsTouched => count > 0;
+
+    /// Koordinate des ersten Fingers (0/0 wenn kein Touch).
+    public int X0 => count > 0 ? x[0] : 0;
+    public int Y0 => count > 0 ? y[0] : 0;
+}
+
 public static class Input
 {
     public static bool IsDown(NpadButton button) => false;
     public static bool IsHeld(NpadButton button) => false;
     public static bool IsUp(NpadButton button) => false;
+
+    /// Aktueller Touch-Zustand (bis zu 10 simultane Berührungen).
+    public static TouchState GetTouch() => new TouchState();
+
+    /// Linker Analogstick.
+    public static StickPos GetStickLeft() => new StickPos();
+
+    /// Rechter Analogstick.
+    public static StickPos GetStickRight() => new StickPos();
 }
